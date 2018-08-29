6e610286dff279e066ab0c4fa833ffc31f25363c
Niilo Jaakkola <niilo.jaakkola@icloud.com>
2018-08-29 16:08:09 +0300

    Added frontend boilerplate

---
+++ frontend/src/routes/Root.jsx
@@ -0,0 +1,40 @@
+import React from 'react'
+import PropTypes from 'prop-types'
+
+import {
+  Router,
+  Route,
+  Switch
+} from 'react-router-dom'
+
+import { PersistGate } from 'redux-persist/integration/react'
+
+import { Provider } from 'react-redux'
+
+import history from './history'
+
+import Header from '../containers/HeaderContainer'
+
+const Root = ({ store, persistor }) => (
+  <Provider store={store}>
+    <PersistGate loading={<h1>Loading</h1>} persistor={persistor}>
+      <Router history={history}>
+        <div className="root-container">
+          <Header />
+          <Switch>
+            <Route exact path='/' >
+              <Header />
+            </Route>
+          </Switch>
+        </div>
+      </Router>
+    </PersistGate>
+  </Provider>
+)
+
+Root.propTypes = {
+  store: PropTypes.object.isRequired,
+  persistor: PropTypes.object.isRequired
+}
+
+export default Root
