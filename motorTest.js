2ce73beb68c2735c08a89f93f34ec1d45ff32d2f
Niilo Jaakkola <niilo.jaakkola@icloud.com>
2018-10-04 12:44:51 +0300

    Fixed error and added gpio

--- motorTest.js
+++ motorTest.js
@@ -4,19 +4,24 @@ const pins = gpio.promise
 
 let state = false
 
+const main = async () => {
+    await pins.setup(3, gpio.DIR_OUT)
+    blink().catch(e => {
+        console.log('Error: ', e.toString())
+    })
+}
+
 const sleep = async ms => (
     new Promise(resolve => setTimeout(resolve, ms))
 )
 
 const blink = async () => {
-    await pins.setup(3, gpio.DIR_OUT)
     await pins.write(3, state)
     state != state
     await sleep(500)
     blink()
 }
 
-
-blink().catch(e => {
+main().catch(e => {
     console.log('Error: ', e.toString())
 })
