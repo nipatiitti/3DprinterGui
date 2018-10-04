4bf2523f4585e5b140f6a1b74ae3a08c880d793e
Niilo Jaakkola <niilo.jaakkola@icloud.com>
2018-10-04 12:18:20 +0300

    Added gpio led test

--- motorTest.js
+++ motorTest.js
@@ -1,1 +1,14 @@
-console.log("works")
+import gpio from 'rpi-gpio'
+
+const pins = gpio.promise
+
+const main = async () => {
+    await pins.setup(2, gpio.DIR_OUT)
+    pins.write(2, true)
+}
+
+
+main()
+.catch((err) => {
+    console.log('Error: ', err.toString())
+})
