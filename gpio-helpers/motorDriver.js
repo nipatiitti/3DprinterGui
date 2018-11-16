65d7d5684f86b81db0a51eb2c36fb44b67767299
Niilo Jaakkola <niilo.jaakkola@icloud.com>
2018-11-16 16:02:28 +0200

    Done

--- gpio-helpers/motorDriver.js
+++ gpio-helpers/motorDriver.js
@@ -66,9 +66,9 @@ class MotorDriver {
         return new Promise(async (resolve, reject) => {
             try {
                 await pins.write(this.stepPin, true)
-                //await sleep(this.sleep/this.mode.speed)
+                await sleep(this.sleep/this.mode.speed)
                 await pins.write(this.stepPin, false)
-                //await sleep(this.sleep/this.mode.speed)
+                await sleep(this.sleep/this.mode.speed)
                 resolve({value: 200})
             } catch (e) {
                 reject({value: 500, message: e.toString()})
